d81b38802f7eb34bdcf248d2fcd560ee875974b3
camandbab <afsharbk@gmail.com>
2016-12-17 17:03:26 -0500

    first version of site

---
+++ script.js
@@ -0,0 +1,25 @@
+$('#welcome').show();
+
+$(document).ready(function(e) {
+  function showView(viewName) {
+    $('.view').hide();
+	$('#' + viewName).show();
+  }
+  $('[data-launch-view]').click(function (e) {
+	 e.preventDefault();
+	var viewName = $(this).attr('data-launch-view');
+	showView(viewName);
+  });
+});
+
+
+$(window).scroll(function(){
+    $(".pp").css("opacity", 1 - $(window).scrollTop() / 800);
+  
+  });
+
+
+$(window).scroll(function(){
+    $(".welcomeparallax").css("opacity", 1 - $(window).scrollTop() / 800);
+  
+  });
---
+++ weddingsite_12-16parralax.markdown
@@ -0,0 +1,7 @@
+weddingsite_12.16parralax
+-------------------------
+
+
+A [Pen](https://codepen.io/Bobbin4Apples/pen/jVXYxW) by [Bob Afshar](http://codepen.io/Bobbin4Apples) on [CodePen](http://codepen.io/).
+
+[License](https://codepen.io/Bobbin4Apples/pen/jVXYxW/license).
