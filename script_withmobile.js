ddd48a40636abb6259ee641f612a19b5318161da
camandbab <afsharbk@gmail.com>
2017-05-14 22:27:59 -0400

    padding edits

--- script_withmobile.js
+++ script_withmobile.js
@@ -26,8 +26,8 @@ $(document).ready(function(e) {
 });
 
 $(window).scroll(function(){
-    $(".pp").css("opacity", 1 - $(window).scrollTop() / 800);
+    $(".pp").css("opacity", 1 - $(window).scrollTop() / 700);
   });
 $(window).scroll(function(){
-    $(".welcomeparallax").css("opacity", 1 - $(window).scrollTop() / 800);  
+    $(".welcomeparallax").css("opacity", 1 - $(window).scrollTop() / 700);  
   });
