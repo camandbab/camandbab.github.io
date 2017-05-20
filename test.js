355e81881fc5080fc6908c23ed9f93d6a69694cd
camandbab <afsharbk@gmail.com>
2017-05-20 15:20:22 -0400

    welcome text - working on dynamic

---
+++ test.js
@@ -0,0 +1,33 @@
+$(document).ready(function(e) {
+	$('.view:not(#welcome)').hide();
+	/*$('#welcome').show();	*/
+		
+	if ( screen.width <= 699 || (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) ) {
+	/*mobile page */
+		document.location = "m.html";
+		$('.view:not(#welcome)').hide();
+		$( ".cross" ).hide();
+		$( ".menu" ).hide();
+	}
+	else {
+	/*desktop page */
+	function showView(viewName) {
+		$('.view').hide();
+		$('#' + viewName).show();
+		$(window).scrollTop(0);
+	  }
+	  $('[data-launch-view]').click(function (e) {
+		 e.preventDefault();
+		var viewName = $(this).attr('data-launch-view');
+		showView(viewName);
+	  });
+	}
+
+});
+
+$(window).scroll(function(){
+    $(".pp").css("opacity", 1 - $(window).scrollTop() / 700);
+  });
+$(window).scroll(function(){
+    $(".welcomeparallax").css("opacity", 1 - $(window).scrollTop() / 700);  
+  });
